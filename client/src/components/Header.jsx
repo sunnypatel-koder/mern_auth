9448bdd5ff0b24165e5f5c7349ae33eeb926547a
Ved_Patel <musicloverved37@gmail.com>
2024-01-22 00:21:27 +0530

    update header component and make profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,8 +18,16 @@ const Header = () => {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-up">
-            <li>Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="h-7 w-7 rounded-full object-cover"
+                src={currentUser.profilePicture}
+                alt=""
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
