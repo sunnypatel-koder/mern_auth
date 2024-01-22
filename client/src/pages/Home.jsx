c49932b32e6307afe183194235e961ccf3eabbe8
Ved_Patel <musicloverved37@gmail.com>
2024-01-22 23:11:50 +0530

    complete home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-gray-100 h-screen flex justify-center">
+      <div className="text-center mt-9 max-w-xl">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
+        <p className="text-lg text-gray-700 mb-8">
+          Thank you for visiting! Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.
+        </p>
+        <p className="text-lg text-gray-700">
+          Explore our content and discover amazing things. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
